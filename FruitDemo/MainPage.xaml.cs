62f5a4aab2a8773cab7782617fd4e53f99ec595c
nic00la1 <nic00la.kaleta@gmail.com>
2023-11-12 09:08:16 +0100

    list

--- FruitDemo/MainPage.xaml.cs
+++ FruitDemo/MainPage.xaml.cs
@@ -9,7 +9,37 @@ namespace FruitDemo
         public MainPage()
         {
             InitializeComponent();
-            PapayaInfo = new List<FruitInformation>();
+            PapayaInfo = new List<FruitInformation>
+          {
+               new FruitInformation
+               {
+                    Micronutrient = "Vitamin C",
+                    Percentage = 101
+               },
+               new FruitInformation
+               {
+                    Micronutrient = "Vitamin A",
+                    Percentage = 19
+               },
+               new FruitInformation
+               {
+                    Micronutrient = "Calcium",
+                    Percentage = 2
+               },
+               new FruitInformation
+               {
+                    Micronutrient = "Magnesium",
+                    Percentage = 5
+               },
+               new FruitInformation
+               {
+                    Micronutrient = "Potassium",
+                    Percentage = 5
+               },
+          };
+            this.BindingContext = this;
 
+
+        }
     }
 }
