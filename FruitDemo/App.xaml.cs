4fc5661318b0d3a206389889142327c7f148b5d0
nic00la1 <nic00la.kaleta@gmail.com>
2023-11-12 08:54:05 +0100

    Add project files.

---
+++ FruitDemo/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace FruitDemo
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
